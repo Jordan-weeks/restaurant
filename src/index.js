836d6b5be40148caa44bd554c7cb1188491f522d
Jordan <jdweeks2010@gmail.com>
2022-06-12 18:57:33 -0700

    changed file organization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {build as buildNav} from "./nav.js"
 import { buildHome } from "./home.js"
+import { buildMenu } from "./menu.js"
 import "./styles.css"
 
 buildNav()
 // buildHome()
+buildMenu()
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,44 @@
 const buildMenu = () =>{
+  //create beer cards
+  // class beerFactory {
+  //   constructor(beerName, abv, ibu, description){
+  //     this.beerName = beerName
+  //     this.abv = abv
+  //     this. ibu = ibu
+  //     this.description = description
+  //   }
+    let tapList = [{
+      beerName: "Fresh Haze IPA",
+      ibu: "45",
+      abv: "6.5%",
+      description: "Soak up the soft golden rays of an inviting malt body as the lively hues of citrusy hops linger on the horizon of a cool, refreshing finish."
+      },
+      {
+      beerName: "Black Butte Porter",
+      ibu: "30",
+      abv: "5.5%",
+      description: "Bold reputation. Soft disposition. Surprising Balance. Behold an iconic and unexpectedly complex porter that’s more than meets the eye."
+      }
+  ]
+    const buildCard = (beerName,abv,ibu,description)=> {
+      let card = document.createElement("div")
+      let beerNameElement = document.createElement("h1")
+      let abvElement = document.createElement("p")
+      let ibuElement = document.createElement("p")
+      let descriptionElement = document.createElement("p")
+
+      card.classList.add("beer-card")
+      
+      beerNameElement.innerText=`${beerName}`
+      abvElement.innerText=`ABV: ${abv}`
+      ibuElement.innerText=`IBU: ${ibu}`
+      descriptionElement.innerText=`${ description }`
+
+      card.appendChild(beerNameElement,abvElement,ibuElement,descriptionElement)
+
+    }
+  
+  
       // create DOM elements
   let content = document.getElementById("content")
   let hero = document.createElement("div")
@@ -13,11 +53,12 @@ const buildMenu = () =>{
   heroText.appendChild(h2)
   
   //apply classes to elements
-  hero.classList.add("hero")
+  hero.classList.add("menu")
   heroText.classList.add("hero-text")
   
   // apply inner text
-  h1.innerText = "Big Brew"
-  h2.innerText = "Tap house"
+  h1.innerText = "The Brews"
+  
   
 }
+export {buildMenu}
